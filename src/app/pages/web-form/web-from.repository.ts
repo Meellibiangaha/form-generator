456bf7533f24995ef7f421c874d8c9dc2b7b3250
Dmitriy <dima-25500@mail.ru>
2024-11-04 00:19:53 +0300

    типизация для WebFormRepository

--- src/app/pages/web-form/web-from.repository.ts
+++ src/app/pages/web-form/web-from.repository.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { JsonFormDataConfig } from '../../core/config/json-form-data';
+import { JsonForm } from '../../core/models/json-form';
 
 @Injectable({
   providedIn: 'any',
 })
 export class WebFormRepository {
   constructor(private http: HttpClient) {}
-  loadForm(): Observable<any> {
+  loadForm(): Observable<JsonForm> {
     return of(JsonFormDataConfig.jsonFormOne);
   }
 }
